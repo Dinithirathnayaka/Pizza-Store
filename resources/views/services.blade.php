9473b0f5616774d91653254a2e9f3a3d3cd4bc14
Dinithirathnayaka <dinithinayanamali98@gmail.com>
2023-07-23 00:23:31 +0530

    add some style to service page

--- resources/views/services.blade.php
+++ resources/views/services.blade.php
@@ -51,57 +51,60 @@
     </section>
 
 
-    <section class="ftco-section">
+    <section class="section-service">
     	<div class="container">
     		<div class="row justify-content-center mb-5 pb-3">
-          <div class="col-md-7 heading-section ftco-animate text-center">
-            <h2 class="mb-4">Hot Meals</h2>
+          <div class="col-md-7 heading-section e text-center">
+            <h2 class="" style="color: #ffffff">Hot Meals</h2>
             <p class="flip"><span class="deg1"></span><span class="deg2"></span><span class="deg3"></span></p>
-            <p class="mt-5">Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
+            <p class="mt-5" style="color: #808080">Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
           </div>
         </div>
     		<div class="row">
-    			<div class="col-md-3 text-center ftco-animate">
+    			<div class="col-md-3 text-center ">
       			<div class="menu-wrap">
-      				<a href="#" class="menu-img img mb-4" style="background-image: url(images/pizza-1.jpg);"></a>
+					<img src="images/pizza-1.jpg" class="menu-img"  />
+      				
       				<div class="text">
-      					<h3><a href="#">Itallian Pizza</a></h3>
-      					<p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia.</p>
-      					<p class="price"><span>$2.90</span></p>
-      					<p><a href="#" class="btn btn-white btn-outline-white">Add to cart</a></p>
+      					<h5>Itallian Pizza</h5>
+      					<p style="color: #808080">Far far away, behind the word mountains, far from the countries Vokalia and Consonantia.</p>
+      					<p class="price"><span style="color: #ffc107">$2.90</span></p>
+      					<p><a href="#" class="btn-service ">Add to cart</a></p>
       				</div>
       			</div>
       		</div>
-      		<div class="col-md-3 text-center ftco-animate">
+      		<div class="col-md-3 text-center ">
       			<div class="menu-wrap">
-      				<a href="#" class="menu-img img mb-4" style="background-image: url(images/pizza-2.jpg);"></a>
+					<img src="images/pizza-2.jpg" class="menu-img" />
+      				
       				<div class="text">
-      					<h3><a href="#">Itallian Pizza</a></h3>
-      					<p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia.</p>
-      					<p class="price"><span>$2.90</span></p>
-      					<p><a href="#" class="btn btn-white btn-outline-white">Add to cart</a></p>
+						<h5>Itallian Pizza</h5>
+      					<p style="color: #808080">Far far away, behind the word mountains, far from the countries Vokalia and Consonantia.</p>
+      					<p class="price"><span style="color: #ffc107"> $2.90</span></p>
+      					<p><a href="#" class="btn-service mb-3">Add to cart</a></p>
       				</div>
       			</div>
       		</div>
-      		<div class="col-md-3 text-center ftco-animate">
+      		<div class="col-md-3 text-center ">
       			<div class="menu-wrap">
-      				<a href="#" class="menu-img img mb-4" style="background-image: url(images/pizza-3.jpg);"></a>
+					<img src="images/pizza-7.jpg" class="menu-img" />
+      				
       				<div class="text">
-      					<h3><a href="#">Itallian Pizza</a></h3>
-      					<p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia.</p>
-      					<p class="price"><span>$2.90</span></p>
-      					<p><a href="#" class="btn btn-white btn-outline-white">Add to cart</a></p>
+						<h5>Itallian Pizza</h5>
+      					<p style="color: #808080">Far far away, behind the word mountains, far from the countries Vokalia and Consonantia.</p>
+      					<p class="price" ><span style="color: #ffc107">$2.90</span></p>
+      					<p><a href="#" class="btn-service mb-3">Add to cart</a></p>
       				</div>
       			</div>
       		</div>
-      		<div class="col-md-3 text-center ftco-animate">
+      		<div class="col-md-3 text-center ">
       			<div class="menu-wrap">
-      				<a href="#" class="menu-img img mb-4" style="background-image: url(images/pizza-4.jpg);"></a>
+					<img src="images/pizza-4.jpg" class="menu-img" />
       				<div class="text">
-      					<h3><a href="#">Itallian Pizza</a></h3>
+						<h5>Itallian Pizza</h5>
       					<p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia.</p>
-      					<p class="price"><span>$2.90</span></p>
-      					<p><a href="#" class="btn btn-white btn-outline-white">Add to cart</a></p>
+      					<p class="price"><span style="color: #ffc107">$2.90</span></p>
+      					<p><a href="#" class="btn-service mb-3">Add to cart</a></p>
       				</div>
       			</div>
       		</div>
