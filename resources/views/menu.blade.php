0a7971ef9e299c8017901e8aac0a4db03d209cc3
Dinithirathnayaka <dinithinayanamali98@gmail.com>
2023-07-23 09:51:13 +0530

    adding menu page

---
+++ resources/views/menu.blade.php
@@ -0,0 +1,166 @@
+@extends('layouts.frontend')
+
+@section('content')
+
+<section class="menupizzaimg">
+    <div class="container">
+        <div class="row justify-content-center mb-5 pb-3">
+      <div class="col-md-7 heading-section  text-center p-3">
+        <h2 class="mb-4 pt-3 pb-3" style="color: #fff">Our Menu</h2>
+        <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
+      </div>
+    </div>
+    </div>
+    
+        <div class="row">
+            <div class="col-md-2 col-sm-6 mx-auto d-block text-center p-3">
+                <div class="services-wrap ">
+                    <img src="images/pizza-1.jpg" class="menupizza"/>
+                </div>
+            </div>
+            <div class="col-md-2 col-sm-6 mx-auto d-block text-div ">
+                <div class="services-wrap ">
+                    <div class="text">
+                        <h3>Greek Pizza</h3>
+                        <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia</p>
+                        <p class="price"><span>$2.90</span> <a href="#" class="ml-2 btn btn-white btn-outline-white">Order</a></p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-2 col-sm-6 mx-auto d-block text-center p-3  ">
+                <div class="services-wrap ">
+                    <img src="images/pizza-3.jpg" class="menupizza"/>
+                </div>
+            </div>
+
+            <div class="col-md-2 col-sm-6 mx-auto d-block text-div">
+                <div class="services-wrap ">
+                    <div class="text">
+                        <h3>American Pizza</h3>
+                        <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia </p>
+                        <p class="price"><span>$2.90</span> <a href="#" class="ml-2 btn btn-white btn-outline-white">Order</a></p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-2 col-sm-6 mx-auto d-block text-center p-3">
+                <div class="services-wrap ">
+                    <img src="images/pizza-5.jpg" class="menupizza" />
+                </div>
+            </div>
+            <div class="col-md-2 col-sm-6 mx-auto d-block text-div">
+                <div class="services-wrap ">
+                    <div class="text">
+                        <h3>Margherita</h3>
+                        <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia</p>
+                        <p class="price"><span>$2.90</span> <a href="#" class="ml-2 btn btn-white btn-outline-white">Order</a></p>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="row ">
+          
+            <div class="col-md-2 col-sm-6 mx-auto d-block text-div ">
+                <div class="services-wrap ">
+                    <div class="text">
+                        <h3>Greek Pizza</h3>
+                        <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia</p>
+                        <p class="price"><span>$2.90</span> <a href="#" class="ml-2 btn btn-white btn-outline-white">Order</a></p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-2 col-sm-6 mx-auto d-block text-center p-3 ">
+                <div class="services-wrap ">
+                    <img src="images/pizza-4.jpg" class="menupizza"/>
+                </div>
+            </div>
+            <div class="col-md-2 col-sm-6 mx-auto d-block text-div">
+                <div class="services-wrap ">
+                    <div class="text">
+                        <h3>American Pizza</h3>
+                        <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia </p>
+                        <p class="price"><span>$2.90</span> <a href="#" class="ml-2 btn btn-white btn-outline-white">Order</a></p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-2 col-sm-6 mx-auto d-block text-center p-3 ">
+                <div class="services-wrap ">
+                    <img src="images/pizza-5.jpg" class="menupizza"/>
+                </div>
+            </div>
+
+            <div class="col-md-2 col-sm-6 mx-auto d-block text-div">
+                <div class="services-wrap ">
+                    <div class="text">
+                        <h3>Margherita</h3>
+                        <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia</p>
+                        <p class="price"><span>$2.90</span> <a href="#" class="ml-2 btn btn-white btn-outline-white">Order</a></p>
+                    </div>
+                </div>
+            </div>
+
+           
+            <div class="col-md-2 col-sm-6 mx-auto d-block text-center p-3">
+                <div class="services-wrap ">
+                    <img src="images/pizza-6.jpg" mx-auto d-block.jpg" class="menupizza" />
+                </div>
+            </div>
+          
+        </div>
+
+
+        <div class="col-md-6 mx-auto d-block">
+            <div class="pricing-entry  ">
+                <div class="img" style="background-image: url(images/pizza-5.jpg);"></div>
+                <div class="desc pl-3">
+                    <div class=" text align-items-center">
+                        <h3><span>Hawaiian Special</span></h3>
+                        <span class="price">$49.91</span>
+                    </div>
+                    <div class="d-block">
+                        <p>A small river named Duden flows by their place and supplies</p>
+                    </div>
+                </div>
+            </div>
+            <div class="pricing-entry  ">
+                <div class="img" style="background-image: url(images/pizza-6 mx-auto d-block.jpg);"></div>
+                <div class="desc pl-3">
+                    <div class=" text align-items-center">
+                        <h3><span>Ultimate Overload</span></h3>
+                        <span class="price">$20.00</span>
+                    </div>
+                    <div class="d-block">
+                        <p>A small river named Duden flows by their place and supplies</p>
+                    </div>
+                </div>
+            </div>
+            <div class="pricing-entry  ">
+                <div class="img" style="background-image: url(images/pizza-4.jpg);"></div>
+                <div class="desc pl-3">
+                    <div class=" text align-items-center">
+                        <h3><span>Bacon Pizza</span></h3>
+                        <span class="price">$20.00</span>
+                    </div>
+                    <div class="d-block">
+                        <p>A small river named Duden flows by their place and supplies</p>
+                    </div>
+                </div>
+            </div>
+            <div class="pricing-entry  ">
+                <div class="img" style="background-image: url(images/pizza-8.jpg);"></div>
+                <div class="desc pl-3">
+                    <div class=" text align-items-center">
+                        <h3><span>Ham &amp; Pineapple</span></h3>
+                        <span class="price">$20.00</span>
+                    </div>
+                    <div class="d-block">
+                        <p>A small river named Duden flows by their place and supplies</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    </div>
+</section>
+  
+@endsection
