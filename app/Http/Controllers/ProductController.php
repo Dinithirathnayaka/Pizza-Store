16bda642cddab65985817dd281ff021cb1ead0aa
supunnilakshana <supunnilakshana1999@gmail.com>
2023-08-05 15:48:04 +0530

    feat:add data seeders

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -6,5 +6,55 @@ use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
-    //
+
+
+    public function index()
+    {
+
+    }
+
+    public function show($id)
+    {
+
+    }
+
+    public function create()
+    {
+
+    }
+
+    public function store(Request $request)
+    {
+
+    }
+
+    public function edit($id)
+    {
+
+    }
+
+    public function update(Request $request, $id)
+    {
+
+
+    }
+
+    public function destroy($id)
+    {
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
