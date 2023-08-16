d05ab35ce41d70063eb309eb044d046b1d28e7b6
Dinithirathnayaka <dinithinayanamali98@gmail.com>
2023-08-17 00:36:29 +0530

    added slider bar

--- resources/views/user/contact.blade.php
+++ resources/views/user/contact.blade.php
@@ -1,6 +1,76 @@
 @extends('layouts.user')
 
 @section('content')
+<div class="slider" style="background-image: linear-gradient(to bottom, transparent, #231F1F),url(images/bg_1.jpg);">
+    <!-- fade css -->
+    <div class="myslide ">
+        <div class="txt">
+            <p class="text-center">Welcome</p>
+            <h1 class="text-center"> WE COOKED YOUR DESIRED<br>PIZZA RECIPE</h1>
+            <p class="text-center mb-md-5">A small river named Duden flows by their place and supplies it with the necessary regelialia.</p>
+            <p><a href="#" class="orderbtn mx-auto d-block text-center">Order Now</a> </p>
+        </div>
+
+    </div>
+
+    <div class="myslide ">
+        <div class="txt">
+            <span class="subheading">Crunchy</span>
+            <h1 class="mb-4">Italian Pizza</h1>
+            <p class="mb-4 mb-md-5">A small river named Duden flows by their place and supplies it with the necessary regelialia.</p>
+            <p><a href="#" class="orderbtn mx-auto d-block text-center">Order Now</a> </p>
+        </div>
+        <img src="{{ url('/images/bg_1.png') }}"  class="sliderimg" style="width: 100%; height: 100%;">
+    </div>
+
+    <div class="myslide">
+        <div class="txt">
+            <span class="subheading">Crunchy</span>
+            <h1 class="mb-4">Italian Burger</h1>
+            <p class="mb-4 mb-md-5">A small river named Duden flows by their place and supplies it with the necessary regelialia.</p>
+            <p><a href="#" class="orderbtn mx-auto d-block text-center">Order Now</a> </p>
+        </div>
+        <img src="{{ url('/images/burger-1.jpg') }}"  class="sliderimg" style="width: 100%; height: 100%; border-radius:50% ;">
+    </div>
+
+    <div class="myslide">
+        <div class="txt">
+            <span class="subheading">Delicious</span>
+            <h1 class="mb-4">Italian Pasta</h1>
+            <p class="mb-4 mb-md-5">A small river named Duden flows by their place and supplies it with the necessary regelialia.</p>
+            <p><a href="#" class="orderbtn mx-auto d-block text-center">Order Now</a> </p>
+        </div>
+        <img src="{{ url('/images/pasta-1.jpg') }}"  class="sliderimg" style="width: 100%; height: 100%; border-radius:50% ;">
+    </div>
+
+    <div class="myslide">
+        <div class="txt">
+            <span class="subheading">Delicious</span>
+            <h1 class="mb-4">Lime Juice</h1>
+            <p class="mb-4 mb-md-5">A small river named Duden flows by their place and supplies it with the necessary regelialia.</p>
+            <p><a href="#" class="orderbtn mx-auto d-block text-center">Order Now</a> </p>
+        </div>
+        <img src="{{ url('/images/drink-1.jpg') }}"  class="sliderimg" style="width: 100%; height: 100%; border-radius:50% ;">
+    </div>
+
+
+
+    <!-- /fade css -->
+
+    <!-- onclick js -->
+    <a class="prev" onclick="plusSlides(-1)">&#10094;</a>
+      <a class="next" onclick="plusSlides(1)">&#10095;</a>
+
+    <div class="dotsbox" style="text-align:center">
+        <span class="dot" onclick="currentSlide(1)"></span>
+        <span class="dot" onclick="currentSlide(2)"></span>
+        <span class="dot" onclick="currentSlide(3)"></span>
+        <span class="dot" onclick="currentSlide(4)"></span>
+        <span class="dot" onclick="currentSlide(5)"></span>
+
+    </div>
+
+</div>
     <section class="home-slider owl-carousel img" style="background-image: url(images/bg_1.jpg);">
 
         <div class="slider-item" style="background-image: url(images/bg_3.jpg);">
--- resources/views/user/home.blade.php
+++ resources/views/user/home.blade.php
@@ -1,65 +1,76 @@
 @extends('layouts.user')
 
 @section('content')
-    <section class="home-slider owl-carousel img" style="background-image: url(images/bg_1.jpg);">
-        <div class="slider-item">
-            <div class="overlay"></div>
-            <div class="container">
-                <div class="row slider-text align-items-center" data-scrollax-parent="true">
-
-                    <div class="col-md-6 col-sm-12 ftco-animate">
-                        <span class="subheading">Delicious</span>
-                        <h1 class="mb-4">Italian Cuizine</h1>
-                        <p class="mb-4 mb-md-5">A small river named Duden flows by their place and supplies it with the
-                            necessary regelialia.</p>
-                        <p><a href="#" class="btn btn-primary p-3 px-xl-4 py-xl-3">Order Now</a> <a href="#"
-                                class="btn btn-white btn-outline-white p-3 px-xl-4 py-xl-3">View Menu</a></p>
-                    </div>
-                    <div class="col-md-6 ftco-animate">
-                        <img src="images/bg_1.png" class="img-fluid" alt="">
-                    </div>
-
-                </div>
-            </div>
+<div class="slider" style="background-image: linear-gradient(to bottom, transparent, #231F1F),url(images/bg_1.jpg);">
+    <!-- fade css -->
+    <div class="myslide ">
+        <div class="txt">
+            <p class="text-center">Welcome</p>
+            <h1 class="text-center"> WE COOKED YOUR DESIRED<br>PIZZA RECIPE</h1>
+            <p class="text-center mb-md-5">A small river named Duden flows by their place and supplies it with the necessary regelialia.</p>
+            <p><a href="#" class="orderbtn mx-auto d-block text-center">Order Now</a> </p>
         </div>
 
-        <div class="slider-item">
-            <div class="overlay"></div>
-            <div class="container">
-                <div class="row slider-text align-items-center" data-scrollax-parent="true">
+    </div>
 
-                    <div class="col-md-6 col-sm-12 order-md-last ftco-animate">
-                        <span class="subheading">Crunchy</span>
-                        <h1 class="mb-4">Italian Pizza</h1>
-                        <p class="mb-4 mb-md-5">A small river named Duden flows by their place and supplies it with the
-                            necessary regelialia.</p>
-                        <p><a href="#" class="btn btn-primary p-3 px-xl-4 py-xl-3">Order Now</a> <a href="#"
-                                class="btn btn-white btn-outline-white p-3 px-xl-4 py-xl-3">View Menu</a></p>
-                    </div>
-                    <div class="col-md-6 ftco-animate">
-                        <img src="{{ url('/images/bg_2.png') }}" class="img-fluid" alt="">
-                    </div>
-
-                </div>
-            </div>
+    <div class="myslide ">
+        <div class="txt">
+            <span class="subheading">Crunchy</span>
+            <h1 class="mb-4">Italian Pizza</h1>
+            <p class="mb-4 mb-md-5">A small river named Duden flows by their place and supplies it with the necessary regelialia.</p>
+            <p><a href="#" class="orderbtn mx-auto d-block text-center">Order Now</a> </p>
         </div>
+        <img src="{{ url('/images/bg_1.png') }}"  class="sliderimg" style="width: 100%; height: 100%;">
+    </div>
 
-        <div class="slider-item" style="background-image: url(images/bg_3.jpg);">
-            <div class="overlay"></div>
-            <div class="container">
-                <div class="row slider-text justify-content-center align-items-center" data-scrollax-parent="true">
-
-                    <div class="col-md-7 col-sm-12 text-center ftco-animate">
-                        <span class="subheading">Welcome</span>
-                        <h1 class="mb-4">We cooked your desired Pizza Recipe</h1>
-                        <p class="mb-4 mb-md-5">A small river named Duden flows by their place and supplies it with the
-                            necessary regelialia.</p>
-                        <p><a href="#" class="btn btn-primary p-3 px-xl-4 py-xl-3">Order Now</a> <a href="#"
-                                class="btn btn-white btn-outline-white p-3 px-xl-4 py-xl-3">View Menu</a></p>
-                    </div>
-
-                </div>
-            </div>
+    <div class="myslide">
+        <div class="txt">
+            <span class="subheading">Crunchy</span>
+            <h1 class="mb-4">Italian Burger</h1>
+            <p class="mb-4 mb-md-5">A small river named Duden flows by their place and supplies it with the necessary regelialia.</p>
+            <p><a href="#" class="orderbtn mx-auto d-block text-center">Order Now</a> </p>
         </div>
-    </section>
+        <img src="{{ url('/images/burger-1.jpg') }}"  class="sliderimg" style="width: 100%; height: 100%; border-radius:50% ;">
+    </div>
+
+    <div class="myslide">
+        <div class="txt">
+            <span class="subheading">Delicious</span>
+            <h1 class="mb-4">Italian Pasta</h1>
+            <p class="mb-4 mb-md-5">A small river named Duden flows by their place and supplies it with the necessary regelialia.</p>
+            <p><a href="#" class="orderbtn mx-auto d-block text-center">Order Now</a> </p>
+        </div>
+        <img src="{{ url('/images/pasta-1.jpg') }}"  class="sliderimg" style="width: 100%; height: 100%; border-radius:50% ;">
+    </div>
+
+    <div class="myslide">
+        <div class="txt">
+            <span class="subheading">Delicious</span>
+            <h1 class="mb-4">Lime Juice</h1>
+            <p class="mb-4 mb-md-5">A small river named Duden flows by their place and supplies it with the necessary regelialia.</p>
+            <p><a href="#" class="orderbtn mx-auto d-block text-center">Order Now</a> </p>
+        </div>
+        <img src="{{ url('/images/drink-1.jpg') }}"  class="sliderimg" style="width: 100%; height: 100%; border-radius:50% ;">
+    </div>
+
+
+
+    <!-- /fade css -->
+
+    <!-- onclick js -->
+    <a class="prev" onclick="plusSlides(-1)">&#10094;</a>
+      <a class="next" onclick="plusSlides(1)">&#10095;</a>
+
+    <div class="dotsbox" style="text-align:center">
+        <span class="dot" onclick="currentSlide(1)"></span>
+        <span class="dot" onclick="currentSlide(2)"></span>
+        <span class="dot" onclick="currentSlide(3)"></span>
+        <span class="dot" onclick="currentSlide(4)"></span>
+        <span class="dot" onclick="currentSlide(5)"></span>
+
+    </div>
+
+</div>
+
 @endsection
+
