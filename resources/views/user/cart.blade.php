1279bc8a3c772a3637d1da5c2b6414ccb51dfaea
Dinithirathnayaka <dinithinayanamali98@gmail.com>
2023-08-19 10:42:59 +0530

    WIP

--- resources/views/user/cart.blade.php
+++ resources/views/user/cart.blade.php
@@ -5,123 +5,103 @@
 @section('content')
 
     <div class="cartmain">
-        <h5 class="cart-title">Your Cart</h5>
+
 
         <div class="cart_container">
 
 
             <div class="cartconLeft">
+                <h5 class="cart-title">Your Cart</h5>
                 <hr />
 
                 @php
                     $total = 0.0;
                     $discount = 0.0;
                 @endphp
-                @foreach ($items as $productId => $item)
-                    <div class="cart">
-                        <div class="cartLeft"><img src="{{ asset('images/pasta-1.jpg') }}" class="cartimg" alt="cartimg">
-                        </div>
-                        <div class="cartMiddle">
-                            <p>{{ $item['product']->name }}</p>Rs.{{ $item['product']->price }}<br><br><span
-                                class="cartpreprice">Rs.{{ round($item['product']->price - ($item['product']->price * $item['product']->discount) / 100, 2) }}</span><span
-                                class="discount">{{ $item['product']->discount }}%</span>
-                        </div>
 
-                        <div class="cartRight p-3">
-                            <form action="{{ route('cart.remove', $item['product']) }}" method="POST">
-                                @csrf
-                                @method('DELETE')
-                                <input type="image" src="{{ asset('images/bin.png') }}" alt="Delete" class="bin">
-                            </form>
-
-                            <br />
-                            <div style="display: flex">
-                                <form action="{{ route('cart.update', $item['product']) }}" method="POST">
-                                    @csrf
-                                    @method('PATCH')
-                                    <input type="hidden" name="quantity" value="{{ -1 }}">
-                                    <button class="minmax" type="submit">-</button>
-                                </form>
-
-                                {{ $item['quantity'] }}
+<table class="carttable"  style="width: 100%">
 
 
-                                <form action="{{ route('cart.update', $item['product']) }}" method="POST">
-                                    @csrf
-                                    @method('PATCH')
-                                    <input type="hidden" name="quantity" value="{{ 1 }}">
-                                    <button class="minmax" type="submit">+</button>
-                                </form>
-                            </div>
-                        </div>
-                    </div>
-                    <hr />
-                    @php
-                        $total += $item['product']->price;
-                        $discount += $item['product']->price * ($item['product']->discount / 100);
-                    @endphp
-                @endforeach
+    <thead class="mb-5" style="margin-bottom: 50px">
+        <tr>
+            <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 text-center mb-3"></th>
+            <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 ps-2 text-center mb-3">Product Details</th>
+            <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 text-center mb-3">Quantity</th>
+            <th class="text-secondary opacity-7 text-center mb-3">Price</th>
+            <th class="text-secondary opacity-7 text-center mb-3">Total</th>
+
+        </tr>
+
+    </thead>
+
+
+    @foreach ($items as $productId => $item)
+        <tbody  class="mt-3">
+
+            <tr class="contenttr">
+                <td class="text-center "><img src="{{ asset('images/pasta-1.jpg') }}" class="cartimgs" alt="cartimg" ></td>
+                <td class="text-center"><p>{{ $item['product']->name }}</p><br><p class="discount">{{ $item['product']->discount }}%</p></td>
+                <td class="text-center">
+                    <div class="text-center quantiyselector">
+                    <form action="{{ route('cart.update', $item['product']) }}" method="POST">
+                        @csrf
+                        @method('PATCH')
+                        <input type="hidden" name="quantity" value="{{ -1 }}">
+                        <button class="minmax" type="submit">-</button>
+                    </form>
+
+                    {{ $item['quantity'] }}
+
+
+                    <form action="{{ route('cart.update', $item['product']) }}" method="POST">
+                        @csrf
+                        @method('PATCH')
+                        <input type="hidden" name="quantity" value="{{ 1 }}">
+                        <button class="minmax" type="submit">+</button>
+                    </form>
+                </div>
+            </td>
+                <td class="text-center">
+                    <p  class="cartpreprice">Rs.{{ round($item['product']->price - ($item['product']->price * $item['product']->discount) / 100, 2) }}
+
+                </td>
+                <td class="text-center"><p>Rs.{{ $item['product']->price }}</td>
+            </tr>
+        </tbody>
+
+        @php
+        $total += $item['product']->price;
+        $discount += $item['product']->price * ($item['product']->discount / 100);
+    @endphp
+    @endforeach
+
+
+
+</table>
+
             </div>
             <div class="cartconRight">
-                <h5>Order Summary</h5>
-                <p class="sub" style="margin-bottom: 20px">Sub Total<span class="ordersum">Rs.
-                        {{ round($total, 2) }}</span></p>
-                <hr />
-                <p class="sub" style="margin-bottom: 20px">Discount<span class="ordersum">Rs.
-                        - {{ round($discount, 2) }}</span></p>
-                <hr />
-                <p>Total<span class="ordersum">Rs. {{ round($total - $discount, 2) }}</span></p>
-                <a href="{{ route('orders.create') }}"><button class="placeorderbtn ">Place
-                        Order</button></a>
+                <h5 style="margin-top: 80px">Order Summary</h5>
 
+                <div class="row text-center">
+                    <div class="col-6"><p class="sub" >Sub Total</p></div>
+                    <div class="col-6"><p>Rs.{{ round($total, 2) }}</p></div>
+                </div>
+                <div class="row text-center">
+                    <div class="col-6"><p class="sub" >Discount</p></div>
+                    <div class="col-6"><p>-Rs.{{ round($discount, 2) }}</p></div>
+                </div>
+                <div class="row text-center">
+                    <div class="col-6"><p class="sub" >Total</p></div>
+                    <div class="col-6"><p>Rs. {{ round($total - $discount, 2) }}</p></div>
+                </div>
+                <a href="{{ route('orders.create') }}" class="text-center"><button class="placeorderbtn ">Place Order</button></a>
+            </div>
 
             </div>
 
 
 
-            {{-- <table class="table text-center" border="2" style="width: 100%">
-             --}}
-            {{-- <thead>
-                <tr>
-                    <th></th>
-                    <th></th>
-                    <th></th>
-                </tr>
-            </thead> --}}
-            {{-- <tbody>
-                @foreach ($items as $productId => $item)
-                    <tr>
-                        <td>{{ $item['product']->name }}</td>
-                        <td>
-                            <form action="{{ route('cart.update', $item['product']) }}" method="POST">
-                                @csrf
-                                @method('PATCH')
-                                <input type="number" name="quantity" value="{{ $item['quantity'] }}">
-                                <button type="submit">Update</button>
-                            </form>
-                        </td>
-                        <td>
-                            <form action="{{ route('cart.remove', $item['product']) }}" method="POST">
-                                @csrf
-                                @method('DELETE')
-                                <button type="submit">Remove</button>
-                            </form>
-                        </td>
-                    </tr>
-                @endforeach
-            </tbody> --}}
-            {{-- </table> --}}
-
-
-            {{-- <form action="{{ route('cart.clear') }}" method="POST" class="text-end pb-5">
-            @csrf
-            @method('DELETE')
-            <button type="submit" class="clear-btn">Clear Cart</button>
-        </form>
-        @if (!$items->isEmpty())
-
-            <a href="{{ route('orders.create') }}" class="addbtn">Complete Order</a>
-        @endif --}}
         </div>
 
     </div>
